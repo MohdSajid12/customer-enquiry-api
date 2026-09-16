c188827ab206986fd736b8506d63e34c3e36a17b
Sajid Shaikh <sajidsultan312@gmail.com>
2026-09-16 13:19:56 +0530

    adding resend api to mail

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,32 +1,5 @@
-// const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
-// const transporter = nodemailer.createTransport({
-//     service: "gmail",
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-//     auth: {
-//         user: process.env.MAIL_USER,
-//         pass: process.env.MAIL_PASS
-//     }
-// });
-
-// module.exports = transporter;
-
-const nodemailer = require("nodemailer");
-
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.MAIL_USER,
-        pass: process.env.MAIL_PASS
-    }
-});
-
-transporter.verify((error, success) => {
-    if (error) {
-        console.error("SMTP VERIFY ERROR:", error);
-    } else {
-        console.log("SMTP SERVER IS READY");
-    }
-});
-
-module.exports = transporter;
+module.exports = resend;
