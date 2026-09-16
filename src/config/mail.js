7c6c5be130a6add2429f5f0a7f5d227ad24bb873
Sajid Shaikh <sajidsultan312@gmail.com>
2026-09-16 12:55:45 +0530

    checking logs

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,11 +1,31 @@
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//     service: "gmail",
+
+//     auth: {
+//         user: process.env.MAIL_USER,
+//         pass: process.env.MAIL_PASS
+//     }
+// });
+
+// module.exports = transporter;
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
-
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS
+    }
+});
+
+transporter.verify((error, success) => {
+    if (error) {
+        console.error("SMTP VERIFY ERROR:", error);
+    } else {
+        console.log("SMTP SERVER IS READY");
     }
 });
 
